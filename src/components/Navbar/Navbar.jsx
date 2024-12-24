082bfbb6728498ee30c44bee7ae7ae0a8d3e21b8
ShraddhaAmbekar <147248222+ShraddhaAmbekar@users.noreply.github.com>
2024-12-24 13:37:08 +0530

    navbar fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { NavLink } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const handleCloseOffcanvas = () => {
+    const offcanvasElement = document.querySelector(".offcanvas");
+    if (offcanvasElement) {
+      // Trigger the hide action using Bootstrap's data attributes
+      offcanvasElement.classList.remove("show");
+      document.body.classList.remove("offcanvas-backdrop"); // Remove the backdrop
+    }
+  };
+
   return (
     <nav id="header-nav" className="navbar navbar-expand-lg py-3 bg-light">
       <div className="container">
-        <Link className="navbar-brand" to="/">
+        <NavLink className="navbar-brand" to="/">
           <img src="/assets/images/logo.png" alt="Logo" className="logo" />
-        </Link>
+        </NavLink>
         <button
           className="navbar-toggler"
           type="button"
@@ -30,9 +39,9 @@ const Navbar = () => {
           aria-labelledby="bdNavbarOffcanvasLabel"
         >
           <div className="offcanvas-header px-4 pb-0">
-            <Link className="navbar-brand" to="/">
+            <NavLink className="navbar-brand" to="/">
               <img src="/assets/images/logo.png" alt="Logo" className="logo" />
-            </Link>
+            </NavLink>
             <button
               type="button"
               className="btn-close"
@@ -44,27 +53,43 @@ const Navbar = () => {
           <div className="offcanvas-body">
             <ul className="navbar-nav text-uppercase justify-content-lg-center flex-grow-1">
               <li className="nav-item">
-                <Link className="nav-link active" to="/">
+                <NavLink
+                  className="nav-link"
+                  to="/"
+                  onClick={handleCloseOffcanvas}
+                >
                   Home
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">
+                <NavLink
+                  className="nav-link"
+                  to="/about"
+                  onClick={handleCloseOffcanvas}
+                >
                   About
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/shop">
+                <NavLink
+                  className="nav-link"
+                  to="/shop"
+                  onClick={handleCloseOffcanvas}
+                >
                   Shop
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/blog">
+                <NavLink
+                  className="nav-link"
+                  to="/blog"
+                  onClick={handleCloseOffcanvas}
+                >
                   Blog
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item dropdown">
-                <Link
+                <NavLink
                   className="nav-link dropdown-toggle"
                   to="#"
                   id="navbarDropdown"
@@ -73,46 +98,72 @@ const Navbar = () => {
                   aria-expanded="false"
                 >
                   Pages
-                </Link>
+                </NavLink>
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                   <li>
-                    <Link className="dropdown-item" to="/about">
+                    <NavLink
+                      className="dropdown-item"
+                      to="/about"
+                      onClick={handleCloseOffcanvas}
+                    >
                       About
-                    </Link>
+                    </NavLink>
                   </li>
                   <li>
-                    <Link className="dropdown-item" to="/shop">
+                    <NavLink
+                      className="dropdown-item"
+                      to="/shop"
+                      onClick={handleCloseOffcanvas}
+                    >
                       Shop
-                    </Link>
+                    </NavLink>
                   </li>
-                 
                   <li>
-                    <Link className="dropdown-item" to="/cart">
+                    <NavLink
+                      className="dropdown-item"
+                      to="/cart"
+                      onClick={handleCloseOffcanvas}
+                    >
                       Cart
-                    </Link>
+                    </NavLink>
                   </li>
                   <li>
-                    <Link className="dropdown-item" to="/checkout">
+                    <NavLink
+                      className="dropdown-item"
+                      to="/checkout"
+                      onClick={handleCloseOffcanvas}
+                    >
                       Checkout
-                    </Link>
+                    </NavLink>
                   </li>
                   <li>
-                    <Link className="dropdown-item" to="/blog">
+                    <NavLink
+                      className="dropdown-item"
+                      to="/blog"
+                      onClick={handleCloseOffcanvas}
+                    >
                       Blog
-                    </Link>
+                    </NavLink>
                   </li>
-                  
                   <li>
-                    <Link className="dropdown-item" to="/contact">
+                    <NavLink
+                      className="dropdown-item"
+                      to="/contact"
+                      onClick={handleCloseOffcanvas}
+                    >
                       Contact
-                    </Link>
+                    </NavLink>
                   </li>
                 </ul>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/contact">
+                <NavLink
+                  className="nav-link"
+                  to="/contact"
+                  onClick={handleCloseOffcanvas}
+                >
                   Contact
-                </Link>
+                </NavLink>
               </li>
             </ul>
           </div>
